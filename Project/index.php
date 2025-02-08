87ba49695abbf33ba74c15189900420c98e792a7
vadim <vadimtop228pc@gmail.com>
2025-02-08 12:04:34 +0300

    first commit

--- Project/index.php
+++ Project/index.php
@@ -1,12 +1,32 @@
 <?php
     class Cat{
-        public $name;
-        public $color
-        public $weight
+        private $name;
+        public $color;
+        public $weight;
+
+        function __construct(string $name, string $color, int $weight){
+            $this -> name = $name;
+            $this -> color= $color;
+            $this -> weight = $weight;
+        
+        }
+
+        function setName(string $name){
+            $this -> name = $name;
+        }
+        function getName(){
+            return $this -> name;
+        }
     }
-    $cat1 new Cat;
-    $cat1 -> color = 'gray';
-    $cat1 -> name = 'iriska';
-    $cat1 -> weight = 5; 
+
+    $cat1 = new Cat('iriska', 'grey', 3);
+    // $cat1 = new Cat;
+    // $cat2 = new Cat;
+    // $cat2 -> setName('cherry');
+    // $cat1 -> color = 'gray';
+    // $cat1 -> setName ('iriska');
+    // $cat1 -> weight = 5; 
+    echo $cat1->getName().'<BR>';
+    // echo $cat2->getName().'<BR>';
     var_dump($cat1);
 ?> 
