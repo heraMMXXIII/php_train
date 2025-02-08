caadd0797577812fb1f034d981bd88916b5ae9d5
vadim <vadimtop228pc@gmail.com>
2025-02-08 11:41:40 +0300

    first commit

---
+++ Project/index.php
@@ -0,0 +1,12 @@
+<?php
+    class Cat{
+        public $name;
+        public $color
+        public $weight
+    }
+    $cat1 new Cat;
+    $cat1 -> color = 'gray';
+    $cat1 -> name = 'iriska';
+    $cat1 -> weight = 5; 
+    var_dump($cat1);
+?> 
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$a = 27; 
+$b = 12;
+$c = round(sqrt($a*$a + $b*$b), 2)
+print "C = $c<BR>"
