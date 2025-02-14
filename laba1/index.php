725a6f220809a7cc89c4d95411a6810d390efeee
vadim <vadimtop228pc@gmail.com>
2025-02-14 15:49:46 +0300

    first hw

---
+++ laba1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Горяев Вадим ДЗ 1</title>
+</head>
+<body>
+    <header>
+        <img src = "Logo_Polytech_rus_main.jpg" style = "height: 100px;    margin-right: 20%;"></img>
+        <h1>"Hello, World!"</h1>
+    </header>
+    <main>
+        <p>
+            <?php echo rand(1,100)
+            ?>
+        </p>
+    </main>
+    <footer>
+        <p>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий. </p>
+</footer>
+</body>
+</html>
