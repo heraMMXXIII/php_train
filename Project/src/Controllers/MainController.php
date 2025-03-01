ab527c56e466c146b7c136d220e18a33d3d8df5d
vadim <vadimtop228pc@gmail.com>
2025-03-01 17:34:38 +0300

    project

--- Project/src/Controllers/MainController.php
+++ Project/src/Controllers/MainController.php
@@ -1,13 +1,28 @@
 <?php
 
 namespace src\Controllers;
+use src\View\View;
 
-class MainController{
-    public function sayHello(string $name){
-        echo 'Hello, '.$name;
+class MainController
+{
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new View(dirname(dirname(__DIR__)) . '/templates');
     }
 
-    public function main(){
-        echo "Главная страница!";
+    public function sayHello(string $name)
+    {
+        $this->view->renderHtml('main/hello.php', ['name' => $name]);
+    }
+
+    public function main()
+    {
+        $articles = [
+            'title' => 'Title 1',
+            'text' => 'Text 1',
+        ];
+        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
     }
 }
---
+++ Project/templates/main/main.php
@@ -0,0 +1,23 @@
+<?php
+require dirname(__DIR__) . '../header.php';
+echo "<h1>Главная старница</h1>";
+?>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($articles as $key => $value): ?>
+            <tr>
+                <th scope="row">1</th>
+                <td><?= $key; ?></td>
+                <td><?= $value; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<?php require dirname(__DIR__) . '/footer.php'; ?>
